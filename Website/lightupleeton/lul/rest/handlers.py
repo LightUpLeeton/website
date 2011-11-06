d3da945e6f245ff5aad9e9d2bdf6f02f09849dbf
Bradley Mclain <BradMclain@users.noreply.github.com>
2011-11-06 10:26:31 +0000

    fixed up bugs in REST and persistent models. basic GET and PUT requests working

--- Website/lightupleeton/lul/rest/handlers.py
+++ Website/lightupleeton/lul/rest/handlers.py
@@ -1,6 +1,6 @@
 from google.appengine.api import users
 from google.appengine.ext import db
-from prestans import handlers, rest
+from prestans import handlers, rest, types
 import lul.models
 import lul.rest.models
 import lul.rest.parsers
@@ -9,44 +9,54 @@ class LocationRESTRequestHandler(handlers.RESTRequestHandler):
     
     request_parser = lul.rest.parsers.LocationRequestParser()
     
-    def create_or_fetch_location(self, address):
-        
-        model = lul.models.Location()
-        
-        return model
-    
     def as_rest_model(self, persistent_model):
         
         rest_model = lul.rest.models.Location()
         
+        rest_model.id = persistent_model.key().id()
         rest_model.address = persistent_model.address
+        rest_model.latitude = persistent_model.location.lat
+        rest_model.longitude = persistent_model.location.lon
         
         return rest_model
         
     def as_rest_models(self, persistent_models):
         
-        rest_models = list()
+        rest_models = types.Array(element_template=lul.rest.models.Location())
         for persistent_model in persistent_models:
             rest_models.append(self.as_rest_model(persistent_model))
         return rest_models
 
     def get(self, location_id):
+        location_query = lul.models.Location.all()
+        locations = location_query.fetch(location_query.count())
+        
         self.response.http_status = rest.STATUS.OK
-        self.response.set_body_attribute('message', 'Location REST')
+        self.response.set_body_attribute('results', self.as_rest_models(locations))
+    
     def post(self, location_id):
         location_rest = self.request.parsed_body_model
-    
-        location_persistent = self.create_or_fetch_location(location_rest.address)
-        location_persistent.address = location_rest.address
+        
+        location_query = lul.models.Location.all().filter("address =", location_rest.address)
+        locations = location_query.fetch(location_query.count())
+        
+        if len(locations) > 0:
+            location_persistent = locations[0]
+        else:
+            location_persistent = lul.models.Location(
+                address=location_rest.address
+            )
+        
         location_persistent.location = db.GeoPt(location_rest.latitude, location_rest.longitude)
         
         #Check if user is logged in, then it must be submitted by committee
         if users.get_current_user():
             location_persistent.added_by_committee = True
-    
+            
+        location_persistent.put()
     
         self.response.http_status = rest.STATUS.OK
-        self.response.set_body_attribute('results', location_rest)
+        self.response.set_body_attribute('results', self.as_rest_models([location_persistent]))
     def put(self, location_id):
         location_rest = self.request.parsed_body_model
     
--- Website/lightupleeton/lul/rest/models.py
+++ Website/lightupleeton/lul/rest/models.py
@@ -1,7 +1,12 @@
 from prestans import types
 
 class Location(types.Model):
+    id = types.Integer(required=False, identifier=True)
     address = types.String(required=True)
     latitude = types.Float(required=True)
     longitude = types.Float(required=True)
     current = types.Boolean(required=True, default=False)
+    
+class Test(types.Model):
+    id = types.Integer(required=True, default=1)
+    
