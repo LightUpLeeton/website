cc38798d1de21a76d04dff5848b9e6f16b4fb56f
Bradley Mclain <BradMclain@users.noreply.github.com>
2013-11-27 07:28:11 +0000

    Updated icon size.

--- Website/lightupleeton/static/js/lul-web.js
+++ Website/lightupleeton/static/js/lul-web.js
@@ -254,7 +254,7 @@ function addMarkersToMap(opt_initial) {
         
             var latLng_ = new google.maps.LatLng(locations[index]["latitude"], locations[index]["longitude"]);
             
-            var image_ = (locations[index]["current"] ? "/img/ornament_green.png" : "/img/ornament_red.png");
+            var image_ = (locations[index]["current"] ? "/img/ornament_green_small.png" : "/img/ornament_red_small.png");
             
             var marker_ = new google.maps.Marker({
                 'map': map,
